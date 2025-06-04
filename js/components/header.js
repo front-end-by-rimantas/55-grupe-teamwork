5cce8deeaab30c3e9a71a0a83e5fe74d3aeb3515
Rimantas <belauzas@gmail.com>
2025-06-04 09:48:32 +0300

    Mobile header

--- js/components/header.js
+++ js/components/header.js
@@ -14,9 +14,54 @@ export function header() {
                     <i class="fa fa-bars hamburger"></i>
                 </div>
             </div>
-        </header>`;
+        </header>
+        <div class="mobile-main-header">
+            <img class="logo" src="./img/logo.webp" alt="Logo">
+            <nav class="main-nav">
+                <a class="link" href="#">Link</a>
+                <a class="link" href="#">Link</a>
+                <a class="link" href="#">Link</a>
+                <a class="link" href="#">Link</a>
+                <a class="link" href="#">Link</a>
+            </nav>
+            <div class="details-group">
+                <p class="details-title">Contact info</p>
+                <ul class="details-list">
+                    <li class="details-item"><i class="fa fa-globe"></i> Turinys</li>
+                    <li class="details-item"><i class="fa fa-globe"></i> Turinys</li>
+                </ul>
+            </div>
+            <div class="details-group">
+                <p class="details-title">Our location</p>
+                <ul class="details-list">
+                    <li class="details-item"><i class="fa fa-globe"></i> Turinys</li>
+                </ul>
+            </div>
+            <div class="details-group">
+                <p class="details-title">Social links</p>
+                <ul class="details-socials">
+                    <a class="fa fa-globe details-icon" href="#" target="_blank"></a>
+                    <a class="fa fa-globe details-icon" href="#" target="_blank"></a>
+                    <a class="fa fa-globe details-icon" href="#" target="_blank"></a>
+                    <a class="fa fa-globe details-icon" href="#" target="_blank"></a>
+                </ul>
+            </div>
+        </div>`;
 
     document
         .getElementById('app')
         .insertAdjacentHTML('beforeend', HTML);
+
+    const hamburgerDOM = document.querySelector('.hamburger');
+    const mobileHeaderDOM = document.querySelector('.mobile-main-header');
+
+    hamburgerDOM.addEventListener('click', () => {
+        mobileHeaderDOM.classList.add('show');
+    });
+
+    addEventListener('keydown', event => {
+        if (event.key === 'Escape') {
+            mobileHeaderDOM.classList.remove('show');
+        }
+    });
 }
