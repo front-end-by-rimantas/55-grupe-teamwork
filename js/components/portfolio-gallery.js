98c0dad8f8a15d75fdd2f24104a7cf5d72807be8
Rimantas <belauzas@gmail.com>
2025-06-09 09:38:56 +0300

    Gallery WIP

--- js/components/portfolio-gallery.js
+++ js/components/portfolio-gallery.js
@@ -1,14 +1,30 @@
 import { portfolioData } from "../data/portfolioData.js";
+import { capitalize } from "../lib/capitalize.js";
 
 export function portfolioGallery() {
+    const uniqueTags = [];
     let cardsHTML = '';
+    let filterHTML = '';
 
     for (const card of portfolioData) {
         cardsHTML += `
             <div class="card">
                 <img class="img" src="./img/services/${card.img}" alt="Portfolio work">
                 <p class="title">${card.title}</p>
+                <p>${card.tags.join(', ')}</p>
             </div>`;
+
+        for (const tag of card.tags) {
+            if (!uniqueTags.includes(tag)) {
+                uniqueTags.push(tag);
+            }
+        }
+    }
+
+    uniqueTags.sort();
+
+    for (const tag of uniqueTags) {
+        filterHTML += `<button>${capitalize(tag)}</button>`;
     }
 
     const HTML = `
@@ -21,11 +37,32 @@ export function portfolioGallery() {
                 </div>
             </div>
             <div class="row">
-                <div class="col-12 portfolio-gallery">${cardsHTML}</div>
+                <div class="col-12 gallery-filter">
+                    <button class="active">All</button>
+                    ${filterHTML}
+                </div>
+                <div class="col-12 gallery-content">${cardsHTML}</div>
             </div>
         </section>`;
 
     document
         .getElementById('app')
         .insertAdjacentHTML('beforeend', HTML);
+
+    const buttonsDOM = document.querySelectorAll('.gallery-filter button');
+    const cardsDOM = document.querySelectorAll('.gallery-content .card');
+
+    for (const buttonDOM of buttonsDOM) {
+        buttonDOM.addEventListener('click', () => {
+            console.log(buttonDOM.textContent);
+
+            for (const cardDOM of cardsDOM) {
+                if (true) {
+                    cardDOM.rodyti();
+                } else {
+                    cardDOM.slepti();
+                }
+            }
+        });
+    }
 }
